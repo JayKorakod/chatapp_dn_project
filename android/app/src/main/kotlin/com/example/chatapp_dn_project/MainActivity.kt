6e9153e9325bd801d633618c8894d7f8fdb6a53f
JayKorakod <korakod.kaisri.gmail.com>
2021-06-13 01:08:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/chatapp_dn_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatapp_dn_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'const.dart';
+import 'login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatApp DN Project',
+      theme: ThemeData(
+        primaryColor: themeColor,
+      ),
+      home: LoginScreen(title: 'CHATAPP DN'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+  
+}
